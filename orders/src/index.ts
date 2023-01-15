d8f78a45e1f6e5b492b36d71fb2def293a939e3b
ryan <h2rr821@gmail.com>
2023-01-14 21:55:52 -0800

    add start up to orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,8 @@ import { PaymentCreatedListener } from "./events/listeners/payment-created-liste
 
 const start = async () => {
 
+  console.log('Starting up the orders')
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
